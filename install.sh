b5b5caf101b3091d5f7a98859b852809b072cc3e
Mike Gillett <mgillett54@gmail.com>
2021-11-07 21:32:06 -0500

    Remove Atmon
    Clean up install script
    Update brew casks

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-source ~/.dotfiles/helpers.sh
+#! /bin/sh
+
+. "./helpers.sh"
 
 title "Link config files"
 ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
@@ -9,41 +10,25 @@ ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 mkdir -p ~/.hammerspoon
 ln -sf ~/.dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
-mkdir -p ~/.atom
-ln -sf ~/.dotfiles/atom/config.cson ~/.atom/config.cson
 mkdir -p ~/.zsh/cache
 
-if [[ $(uname -s) == Darwin ]]
+if [ "$(uname -s)" = Darwin ]
 then # macOS
   title "macOS specific install"
   sh ~/.dotfiles/macos_install.sh
-elif [[ $(uname -s) == Linux ]]
+elif [ "$(uname -s)" = Linux ]
 then
   title "Linux specific install"
-  $(~/.dotfiles/ubuntu_install.sh)
+  sh "$HOME/.dotfiles/ubuntu_install.sh"
 fi
 
 # Change shell to ZSH
-if [[ $SHELL -ef $(which zsh) ]]
+if [ "$SHELL" -ef "$(which zsh)" ]
 then
-  chsh -s $(which zsh)
+  chsh -s "$(which zsh)"
 fi
 
 title "Install Antibody packages"
 sh ./antibody/install.sh
-
-title "Install atom packages"
-# apm install doesn't natively support skipping already installed packages
-# NOTE: Write apm packages to dotfile config
-# apm list --installed --bare > ~/.dotfiles/atom/package.list
-for package_and_version in `cat ~/.dotfiles/atom/package.list`;
-do
-  # Strip the version number off the end of the package
-  package=${package_and_version%@*}
-  if [[ ! -d "$HOME/.atom/packages/$package" ]]
-  then
-    apm install $package
-  fi
-done
 
 title "Done :)"
